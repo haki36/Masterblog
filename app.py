04a66ee8534e7ec55ec996ab2b56db2cbecd7dfb
haki36 <yildirim.h@hotmail.com>
2026-03-26 15:46:32 +0100

    feat: load blog posts from JSON and render dynamically on index page

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+import json
+import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    file_path = os.path.join(app.root_path, 'data', 'posts.json')
+    with open(file_path, "r", encoding="utf-8") as f:
+        blog_posts = json.load(f)
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
